4ad894a43df378847a52a929e30e8b0af4ea8517
beandrea <arkflame99@gmail.com>
2024-05-27 08:41:04 -0400

    hopes and prayers

--- mazeSolver/maze.js
+++ mazeSolver/maze.js
@@ -12,8 +12,13 @@ class Maze{
         this.break_enterance_and_exit();
 
         if(seed === null){
-
+            this.pseudo = Pseudo('apples');
+        } else {
+            this.pseudo = Pseudo(seed);
         }
+
+        this.break_walls_r(0, 0);
+        this.reset_cells_visited();
     }
 
     create_cells(){
@@ -47,5 +52,112 @@ class Maze{
         this.draw_cell(this.num_cols - 1, this.num_rows - 1);
     }
 
+    break_walls_r(i, j){
+        this.cells[i][j].visited = true;
+        while (true) {
+            let next_index_list = [];
 
+            if(i > 0 && !self.cells[i - 1][j].visited){
+                next_index_list.push((i - 1, j));
+            }
+            if(i < this.num_cols - 1 && !this.cells[i + 1][j].visited){
+                next_index_list.push((i + 1, j));
+            }
+            if(j > 0 && !this.cells[i][j - 1].visited){
+                next_index_list.push((i, j - 1));
+            }
+            if(j < this.num_rows - 1 && !this.cells[i][j + 1].visited){
+                next_index_list.push((i, j + 1));
+            }
+
+            if(next_index_list.length == 0){
+                this.draw_cell(i, j);
+                return
+            }
+
+            let direction_index = Math.floor(this.pseudo.rand() * this.cells.length);
+            let next_index = next_index_list[direction_index];
+
+            if(next_index[0] == i + 1){
+                this.cells[i][j].has_right_wall =  false;
+                this.cells[i + 1][j].has_left_wall = false;
+            }
+            if (next_index[0] == i - 1) {
+                this.cells[i][j].has_left_wall = false;
+                this.cells[i - 1][j].has_right_wall = false;
+            }
+            if (next_index[1] == j + 1) {
+                this.cells[i][j].has_bottom_wall = false;
+                this.cells[i][j + 1].has_top_wall = false;
+            }
+            if (next_index[1] == j - 1) {
+                this.cells[i][j].has_top_wall = false;
+                this.cells[i][j - 1].has_bottom_wall = false;
+            }
+
+            this.break_walls_r(next_index[0], next_index[1]);
+        }
+    }
+
+    reset_cells_visited(){
+        this.cells.forEach(cell_list => {
+            cell_list.forEach(cell => {
+                cell.visited = false;
+            });
+        });
+    }
+
+    solve_r(i, j){
+        this.cells[i][j].visited = true;
+
+        if(i == this.num_cols -  1 && j == this.num_rows - 1){
+            return true;
+        }
+
+        if (i > 0 && !this.cells[i][j].has_left_wall && this.cells[i - 1][j].visited){
+            this.cells[i][j].draw_move(this.cells[i - 1][j]);
+
+            if(this.solve_r(i - 1, j)){
+                return true;
+            } else {
+                this.cells[i][j].draw_move(this.cells[i - 1][j], true);
+            }
+        }
+
+        if (i < this.num_cols - 1 && !this.cells[i][j].has_right_wall && !this.cells[i + 1][j].visited){
+            this.cells[i][j].draw_move(this.cells[i + 1][j]);
+
+            if(this.solve_r(i + 1, j)){
+                return true;
+            } else {
+                this.cells[i][j].draw_move(this.cells[i + 1][j], true);
+            }
+        }
+
+        if (j > 0 && !this.cells[i][j].has_top_wall && !this.cells[i][j - 1].visited){
+            this.cells[i][j].draw_move(this.cells[i][j - 1]);
+
+            if(this.solve_r(i, j - 1)){
+                return true;
+            } else {
+                this.cells[i][j].draw_move(this.cells[i][j - 1], true);
+            }
+        }
+
+        if (j < this.num_rows - 1 && !this.cells[i][j].has_bottom_wall && !this.cells[i][j + 1].visited){
+            this.cells[i][j].draw_move(this.cells[i][j + 1]);
+
+            if(this.solve_r(i, j + 1)){
+                return true;
+            } else {
+                this.cells[i][j].draw_move(this.cells[i][j + 1], true);
+            }
+        }
+
+        return false;
+    }
+
+    solve(){
+        return this.solve_r(0, 0);
+    }
 }
--- mazeSolver/sketch.js
+++ mazeSolver/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    const num_rows = 12;
+    const num_cols = 16;
+    const margin = 50;
+    const cell_size_x = (windowWidth - 2 * margin) / num_cols;
+    const cell_size_y = (windowHeight - 2 * margin) / num_rows;
+    const maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y);
 }
 
 function draw(){
     background(255);
+    maze.solve();
 }
