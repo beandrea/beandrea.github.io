5df1e0ccad59fed951185d3ae9d28eec84769c91
beandrea <arkflame99@gmail.com>
2024-08-14 06:41:10 -0400

    load from env file

--- website/templates/__init__.py
+++ website/templates/__init__.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 from flask import Flask
+import os
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = ''
+    app.config['SECRET_KEY'] = os.getenv("KEY")
