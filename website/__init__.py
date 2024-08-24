92afd500ea03e742ee554920d798cf2acb9bb9e4
Andrea Parker <andreaparker@andreas-mbp.mynetworksettings.com>
2024-08-24 16:46:13 -0400

    i really fucking hope gitignore is working

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,12 @@
-from dotenv import load_dotenv
 from flask import Flask
+from dotenv import load_dotenv
 import os
 
-def create_app():
+def configure():
     load_dotenv()
+
+def create_app():
+    configure()
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("KEY")
     return app
